02d3fc611fe72e53134290312dbcd1e196e222c5
Ravi Gangawane <melayer.ravi@gmail.com>
2019-03-15 12:25:44 +0530

    changes to creste ng Trans

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngTranslate';
+
+  marType
+  ngOnInit(): void {
+    console.log('marType', this.marType)
+
+
+  }
+  updateValue(test) {
+    console.log('space', test)
+
+  }
+  updateLast(test) {
+    console.log('Normal ', test)
+
+  }
 }
